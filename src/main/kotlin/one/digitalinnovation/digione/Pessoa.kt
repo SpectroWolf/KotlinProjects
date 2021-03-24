04a8f91c86e662a185e098bcca159c4747771558
Guilherme Goulart <guigagoulart@hotmail.com>
2021-03-24 10:40:26 -0300

    updated to talk about class members

--- src/main/kotlin/one/digitalinnovation/digione/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digione/Pessoa.kt
@@ -2,15 +2,16 @@ package one.digitalinnovation.digione
 
 class Pessoa {
     var nome: String = "Guilherme"
-
     var cpf: String = "123.123.123-12"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main (){
     val guilherme = Pessoa()
 
-    println(guilherme)
-    println(guilherme.nome)
-    println(guilherme.cpf)
+    println(guilherme.pessoaInfo())
 }
