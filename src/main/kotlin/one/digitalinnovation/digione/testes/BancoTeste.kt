2c58541abdca70e264eebe81df33f1612fa48373
Guilherme Goulart <guigagoulart@hotmail.com>
2021-03-24 11:23:40 -0300

    created bank class and test to it

---
+++ src/main/kotlin/one/digitalinnovation/digione/testes/BancoTeste.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digione.testes
+
+import one.digitalinnovation.digione.Banco
+
+fun main(){
+
+    val digiOneBank = Banco("DigiOne", 12)
+
+    println(digiOneBank.info())
+
+}
