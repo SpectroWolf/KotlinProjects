1abdf5896c1378bcb1c324fcbfd8e52b14615e6f
Guilherme Goulart <guigagoulart@hotmail.com>
2021-03-25 11:18:56 -0300

    created sub classes manager and analyst

---
+++ src/main/kotlin/one/digitalinnovation/digione/Analista.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digione
+
+class Analista(nome: String,
+               cpf: String,
+               salario: Double
+) : Funcionario(nome, cpf, salario) {
+    override fun calculoAuxilio() = salario * 0.1
+}
