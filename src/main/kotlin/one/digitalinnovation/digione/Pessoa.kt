cb7e5f0804bd049e0360f0ff24615f407de0b56c
Guilherme Goulart <guigagoulart@hotmail.com>
2021-03-25 10:50:44 -0300

    uptade to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digione/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digione/Pessoa.kt
@@ -1,17 +1,7 @@
 package one.digitalinnovation.digione
 
-class Pessoa {
-    var nome: String = "Guilherme"
-    var cpf: String = "123.123.123-12"
-    private set
+abstract class Pessoa (
+   val nome: String,
+   val cpf: String
+)
 
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main (){
-    val guilherme = Pessoa()
-
-    println(guilherme.pessoaInfo())
-}
